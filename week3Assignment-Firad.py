463ee6ca01724dbc305f688a812d7f377d5dfd04
firadJo <121764901+firadJo@users.noreply.github.com>
2023-03-06 13:36:08 +0300

    Create week3Assignment-Firad.py

---
+++ week3Assignment-Firad.py
@@ -0,0 +1,28 @@
+# Data Science Tools and Ecosystem
+Introduction :-
+This is my Data Science Tools course peers assginment
+Some of the popular languages that Data Scientists use are:
+  1. Python
+2. R
+3. SQL
+Some of the commonly used libraries used by Data Scientists include:
+  1. Pandas
+2. TensorFlow
+3. KERAS
+| Data Science Tools |
+|--------------------|
+| APACHE SPARK       |
+| GETHUB             |
+| Jupyter Notebooks  |
+### Below are a few examples of evaluating arithmetic expressions in Python
+This a simple arithmetic expression to mutiply then add integers
+print((3*4)+5)
+This will convert 200 minutes to hours by diving by 60
+print(200/60)
+**Objectives:**
+  List popular languages for Data Science
+  - Python
+- R
+- SQL
+## Author
+FIRAD MISMAR
